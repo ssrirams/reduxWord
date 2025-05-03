c8365b77575160f0387874193e1c8326028e83a6
sriram <sriram.subramani.career@gmail.com>
2025-05-03 16:47:35 +0530

    all changes are added

--- App.tsx
+++ App.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { persistor, store } from './src/redux/store'; // ⬅️ Updated path
-import TodoList from './src/screens/TodoList';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import { store, persistor } from './src/redux/store';
+import HomeScreen from './src/screens/HomeScreen';
+import TodoList from './src/screens/TodoList';
+import WebViewScreen from './src/screens/WebViewScreen';
+
+export type RootStackParamList = {
+  Home: undefined;
+  TodoList: undefined;
+  WebView: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <TodoList />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="TodoList" component={TodoList} />
+            <Stack.Screen name="WebView" component={WebViewScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </PersistGate>
     </Provider>
   );
